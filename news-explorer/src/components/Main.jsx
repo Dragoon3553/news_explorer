3f92dbe54c8435c1541e98bf89534667060bdc6a
Drew Woods <c.woodsiii@gmail.com>
2026-06-04 02:23:49 -0700

    add: Main html & css

--- news-explorer/src/components/Main.jsx
+++ news-explorer/src/components/Main.jsx
@@ -1,5 +1,33 @@
+import "../blocks/main.css";
+
 function Main() {
-  return <div>MAIN</div>;
+  return (
+    <main className="main">
+      <h2 className="main__title">What's going on in the world?</h2>
+      <p className="main__subtitle">
+        Find the latest news on any topic and save them in your personal
+        account.
+      </p>
+      <form
+        className="search"
+        id="search-form"
+        action="/search-results"
+        method="get"
+      >
+        <div className="search__container">
+          <input
+            className="search__input"
+            type="search"
+            id="search-input"
+            name="q"
+            placeholder="Enter topic"
+            required
+          />
+          <button className="search-btn">Search</button>
+        </div>
+      </form>
+    </main>
+  );
 }
 
 export default Main;
